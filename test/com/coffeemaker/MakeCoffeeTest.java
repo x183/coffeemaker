0268828c4aff995a69b69a0edb5954c91973c108
Oscar Palm <palmoscar4@gmail.com>
2023-02-23 15:52:31 +0100

    added some more tests for coffeemaker

--- test/com/coffeemaker/MakeCoffeeTest.java
+++ test/com/coffeemaker/MakeCoffeeTest.java
@@ -3,16 +3,40 @@ package com.coffeemaker;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class MakeCoffeeTest {
 
-    CoffeeMaker cm;
+    CoffeeMaker coffeeNoRecipe, coffeeExpensive, coffeeNoIngredients, coffeeNormal;
+    Recipe expensive,resourceIntensive;
     @Before
     public void beforeTesting(){
-        cm = new CoffeeMaker();
+        coffeeNoRecipe = new CoffeeMaker();
+        coffeeExpensive = new CoffeeMaker();
+        coffeeNoIngredients = new CoffeeMaker();
+        coffeeNormal = new CoffeeMaker();
+
+        expensive = new Recipe();
+        resourceIntensive = new Recipe();
+
+        try{
+        expensive.setPrice("100");
+        resourceIntensive.setPrice("5");
+        resourceIntensive.setAmtCoffee("5");
+        resourceIntensive.setAmtMilk("5");
+
+        coffeeNoIngredients.addInventory("0","0","0","0");
+        }catch (Exception e){
+            fail("Oopsie woopsie, someone made a fucky wucky");
+        }
     }
     @Test
     public void makeNoRecipeTest(){
-        assertEquals(10,cm.makeCoffee(1,10));
+        assertEquals(10, coffeeNoRecipe.makeCoffee(1,10));
+    }
+    @Test
+    public void makeCoffeeTooFewCoin(){
+        coffeeExpensive.addRecipe(expensive);
+        assertEquals(10,coffeeExpensive.makeCoffee(0,10));
     }
 }
