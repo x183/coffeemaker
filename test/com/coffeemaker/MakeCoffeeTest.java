975d98073e74ce310a738cabf4a195f3311e11b0
Oscar Palm <palmoscar4@gmail.com>
2023-02-24 14:37:28 +0100

    Achieved full branch coverage on setPrice

--- test/com/coffeemaker/MakeCoffeeTest.java
+++ test/com/coffeemaker/MakeCoffeeTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.fail;
 public class MakeCoffeeTest {
 
     CoffeeMaker coffeeNoRecipe, coffeeExpensive, coffeeNoIngredients, coffeeNormal;
-    Recipe expensive,resourceIntensive;
+    Recipe expensive,resourceIntensive,normalRecipe;
     @Before
     public void beforeTesting(){
         coffeeNoRecipe = new CoffeeMaker();
@@ -18,14 +18,20 @@ public class MakeCoffeeTest {
 
         expensive = new Recipe();
         resourceIntensive = new Recipe();
+        normalRecipe = new Recipe();
 
         try{
         expensive.setPrice("100");
-        resourceIntensive.setPrice("5");
-        resourceIntensive.setAmtCoffee("5");
-        resourceIntensive.setAmtMilk("5");
+        resourceIntensive.setPrice("25");
+        resourceIntensive.setAmtCoffee("25");
+        resourceIntensive.setAmtMilk("25");
+            resourceIntensive.setAmtSugar("25");
+            resourceIntensive.setAmtChocolate("25");
 
-        coffeeNoIngredients.addInventory("0","0","0","0");
+        normalRecipe.setAmtMilk("10");
+            normalRecipe.setAmtCoffee("10");
+            normalRecipe.setPrice("5");
+
         }catch (Exception e){
             fail("Oopsie woopsie, someone made a fucky wucky");
         }
@@ -38,5 +44,16 @@ public class MakeCoffeeTest {
     public void makeCoffeeTooFewCoin(){
         coffeeExpensive.addRecipe(expensive);
         assertEquals(10,coffeeExpensive.makeCoffee(0,10));
+    }
+    @Test
+    public void makeCoffeeNoIngredients(){
+        coffeeNoIngredients.addRecipe(resourceIntensive);
+        assertEquals(30,coffeeNoIngredients.makeCoffee(0,30));
+    }
+    @Test
+    public void makeCoffeeNormal(){
+        coffeeNormal.addRecipe(normalRecipe);
+        assertEquals(5,coffeeNormal.makeCoffee(0,10));
+
     }
 }
