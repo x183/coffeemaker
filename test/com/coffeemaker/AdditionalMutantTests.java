6b84818e7b73f3f1fc71a833a37ba50359a27158
affe4ever <93663446+affe4ever@users.noreply.github.com>
2023-03-08 16:25:56 +0100

    created new testclass

---
+++ test/com/coffeemaker/AdditionalMutantTests.java
@@ -0,0 +1,15 @@
+package com.coffeemaker;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AdditionalMutantTests {
+    @Before
+    public void init(){
+
+    }
+    @Test
+    public void badoo(){
+
+    }
+}
